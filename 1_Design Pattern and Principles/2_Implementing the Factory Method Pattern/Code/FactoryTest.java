5feb26e5c23a232759616efc9ae934016d3f9ca4
Santhosh Abraham <santhoshabraham605@gmail.com>
2025-06-21 20:56:35 +0530

    week1-design pattern and principles-assignment2

---
+++ 1_Design Pattern and Principles/2_Implementing the Factory Method Pattern/Code/FactoryTest.java
@@ -0,0 +1,20 @@
+package factory;
+
+public class FactoryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open(); 
+	}
+
+}
